a4e0ccc4214e8c524aae300640aa799c7e98aed4
Karim <khraibi@gmail.com>
2019-05-07 22:10:17 +0200

    Create pi-ups.ino

---
+++ pi-ups.ino
@@ -0,0 +1,41 @@
+/* 
+ * Uninterrruptible Power Supply (UPS) for a Raspberry Pi
+ * 
+ * This source file is part of the Raspberry Pi UPS Arduino firmware
+ * found under http://www.github.com/microfarad-de/pi-ups
+ * 
+ * Please visit:
+ *   http://www.microfarad.de
+ *   http://www.github.com/microfarad-de
+ * 
+ * Copyright (C) 2019 Karim Hraibi (khraibi at gmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * Version: 2.1.0
+ * Date:    May 2019
+ */
+#define VERSION_MAJOR 1  // major version
+#define VERSION_MINOR 0  // minor version
+#define VERSION_MAINT 0  // maintenance version
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
